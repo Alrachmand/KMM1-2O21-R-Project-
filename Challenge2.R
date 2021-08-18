20039fb134febaa59e558ef7e05c8841a39449cb
Alrachmand <aliefrachmandias123@gmail.com>
2021-08-18 12:04:30 +0700

    initial commit

--- Challenge2.R
+++ Challenge2.R
@@ -2,3 +2,27 @@
 fl2 <- select(fl, year, month, day, dep_delay, arr_delay, carrier, origin, dest, air_time, distance) 
 fl2
 
+## Menggunakan fungsi filter
+# Penerbangan telat berangkat lebih dari 1 jam tapi telah datang kurang dari setengan jam
+filter(fl2, dep_delay >= 60, arr_delay <= 30)
+
+# Penerbangan antara nov - jan 
+filter(fl2, month >= 11 | month == 1)
+filter(fl2, month %in% c(12,11,1))
+
+# Penerbangan dari maskapai UA dan AA
+filter(fl2, carrier == "UA" | carrier == "AA")
+filter(fl2, carrier %in% c("UA","AA"))
+
+# Pilih kolom dep_delay, arr_delay, air_time, distance 
+# Temukan penerbangan dengan berangkat telat lebih 20 menit, tapi datang tidak telat
+# Buat kolom berisi informasi kecepatan dalam satuan mil/jam
+# Urutkan berdasar kecepatan tertinggi
+
+fl6 <- select(fl2, dep_delay, arr_delay, air_time, distance)
+fl7 <- filter(fl6, dep_delay > 20, arr_delay <= 0)
+fl8 <- mutate(fl7, 
+              speed = distance / (air_time/60))
+fl9 <- arrange(fl8, desc(speed))
+
+
--- materi 6,7,9.R
+++ materi 6,7,9.R
@@ -16,7 +16,18 @@ flights
 glimpse(flights)
 fl <- flights
 
-    
+## Operator perbandingan
+# == persamaan
+# != pertidak samaan
+# >  lebih dari
+# >= lebih dari sama dengan
+# <  kurang dari
+# <= kurang dari sama dengan
+## Operator Logika
+# & dan (pilih keduanya)
+# | atau (pilih salah satu kondisi)
+# ! bukan (yg tidak dipilih)
+
 ## Materi Data Processing
   #  1.Memilih kolom dengan select()
         select(fl, year, month, day)
@@ -28,22 +39,10 @@ fl <- flights
         select(fl, starts_with("dep")) ---#Membantu mencari kolom dengan awalan dep
         select(fl, ends_with("time"))  ---#Membantu mencari kolom dengan akhiran time
         select(fl, contains("arr"))    ---#Membatu mencari kolom yang mengandung kata arr di dalamnya
+          
   #  2.Memfilter baris dengan filter()
         #Mencari jadwal terbang u tanggal 17 August  
         filter(fl2, month == 8, day == 17)
-        
-## Operator perbandingan
-   # == persamaan
-   # != pertidak samaan
-   # >  lebih dari
-   # >= lebih dari sama dengan
-   # <  kurang dari
-   # <= kurang dari sama dengan
-## Operator Logika
-   # & dan (pilih keduanya)
-   # | atau (pilih salah satu kondisi)
-   # ! bukan (yg tidak dipilih)
-        
         #Mencari jarak antar bandara lebih dari 1000 miles
         filter(fl2, distance > 1000)
         #Mencari jadwal keberangkatan yang tidak delay
@@ -58,9 +57,40 @@ fl <- flights
         filter(fl2, distance > 999 & distance < 1399)
         #Mencari penerbangan ke Houston (IAH & HOU)
         filter(fl2, dest == "IAH" | dest == "HOU")
-  #  3.Mengurutkan nilai 
+        
+  #  3.Mengurutkan nilai
+        #Mencari penerbangan yang paling dini
+        arrange(fl2, dep_delay)
+        arrange(fl2, desc(dep_delay)) ---#descending(Tinggi - rendah), ascending(Rendah - tinggi)
+        #Mengurutkan berdasarkan huruf
+        arrange(fl2, carrier)
+        #Mengurutkan dari bulan tertinggi dep_delay rendah
+        arrange(fl2, desc(month), dep_delay)
+        
   #  4.Membuat kolom baru
+        #Menkonversi nilai arr_delay jadi satuan detik
+        fl3 <- mutate(fl2, arr_delay_2 = arr_delay * 60)
+        view(mutate(fl2, arr_delay_2 = arr_delay * 60)) --- #lihat kolom baru
+        fl4 <- mutate(fl2, 
+               arr_delay_2 = arr_delay * 60,
+               dep_delay_2 = dep_delay * 60
+               ) 
+        #Menggunakan case_when
+          #Mengelompokan status telat / tidak telat
+          fl5 <- mutate(fl2, 
+                 stats_delay = case_when(dep_delay > 0 ~ "Late",
+                                         dep_delay == 0 ~"On time",
+                                         dep_delay < 0 ~ "Early")
+                 )
+          
   #  5.Menggunakan Operator pipe
+          # %>% atau |>
+          fl10 <- fl2  |>
+            select(dep_delay, arr_delay, air_time, distance) |>
+            filter(dep_delay > 20 & arr_delay <= 0) |>
+            mutate(speed = distance / (air_time / 60)) |>
+            arrange(desc(speed))
+          
   #  6.Agregasi data
   #  7.Prapemrosesan data teks
 
